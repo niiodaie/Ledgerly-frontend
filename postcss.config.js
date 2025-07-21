898642fc2940eee1981caf407d63ab8c959e261c
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-21 03:03:39 -0500

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-import postcssOklabFunction from 'postcss-oklab-function';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [
-    postcssOklabFunction(),  // ✅ Add OKLCH/OKLAB color space support
-    tailwindcss(),
-    autoprefixer(),
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
