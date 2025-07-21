574fd8eb6fad67dcd8c929e4e8350c609cd035d1
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-21 03:16:42 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import postcssOklabFunction from 'postcss-oklab-function';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
+export default {
+  plugins: [
+    postcssOklabFunction(),
+    tailwindcss(),
+    autoprefixer(),
+  ],
+};
